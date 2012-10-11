bbd3893421fbe55da0d47d57b7325a713d681d07
HUmar104 <HUmar104@gmail.com>
2012-10-11 15:44:44 -0400

    Forgot a file in the server

---
+++ Comms/JavaServer/DataOperaion.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+@SuppressWarnings("serial")
+public class DataOperaion implements Serializable{
+
+	public boolean status;
+	public boolean q;
+	public short acceleration;
+	public short cycle;
+	
+	public DataOperaion(boolean flag, boolean quit, short acc, short cyc)
+	{
+		this.status = flag;
+		this.q = quit;
+		this.acceleration = acc;
+		this.cycle = cyc;
+	}
+	public DataOperaion() {
+		// TODO Auto-generated constructor stub
+	}
+	public boolean quit()
+	{
+		return q;
+	}
+	public boolean status()
+	{
+		return status;
+	}
+	public short getAcceleration()
+	{
+		return acceleration;
+	}
+	public short getCycle()
+	{
+		return cycle;
+	}
+    public String OperationDescription()
+    {
+        if(this.status())
+        {
+            return "accelerate at " + this.getAcceleration() + " rpm/s for " + this.getCycle()+ " Cycles.";
+        }
+        return "decelerate at " + this.getAcceleration() + " rpm/s for " + this.getCycle()+ " Cycles.";
+    }
+}
