65b89a9665374c9faf575296e4f48ce59fb07580
HUmar104 <HUmar104@gmail.com>
2012-11-06 15:44:57 -0500

    Two Way communication server

--- Comms/LunarEx_Server/lunarex_server.py
+++ Comms/LunarEx_Server/lunarex_server.py
@@ -4,6 +4,7 @@ import sys
 import serial
 import json
 import time
+import select
 from threading import Thread
 
 HOST=''
@@ -23,35 +24,49 @@ class Handler(SocketServer.BaseRequestHandler):
 
     def setup(self):
         self.currentState = Data(0.0,0.0,0.0)
+
         self.initialTime = int(time.time()*1000.0)
         self.currentTime = int(time.time()*1000.0)
-        self.request.setblocking(1)
+
+        self.request.setblocking(0)
+
         self.datalist = []
-        self.ser=serial.Serial(COM,BAUD,timeout=1)
+        #self.ser=serial.Serial(COM,BAUD,timeout=1)
         self.count=0
+
         print str(self.request.getpeername())+" connected"
 
 
     def handle(self):
         while(True):
-            # self.request is the client connection
-            data = self.request.recv(1)
-            self.datalist.append(data)
-            #if(len(data)==1):
-            while len(self.datalist) > 0:
-                data_in = self.datalist.pop()
-                print 'Recv: ' + str(ord(data_in)) + '; Count: ' + str(self.count)
-                self.ser.write(str(ord(data_in)))
-                self.count+=1
-
-            '''
             self.currentTime = int(time.time()*1000.0)
+
+            # self.request is the client connection
+            try:
+                data = self.request.recv(1)
+                self.datalist.append(data)
+                while(len(self.datalist) > 0):
+                    data_in = self.datalist.pop()
+
+                    if(ord(data_in) == 1):
+                        self.currentState.x+=1
+                    elif(ord(data_in) == 2):
+                        self.currentState.x-=1
+                    elif(ord(data_in) == 4):
+                        self.currentState.y+=1
+                    else:
+                        self.currentState.y-=1
+
+                    #self.ser.write(str(ord(data_in)))
+                    self.count+=1
+            except IOError:
+                pass
+
             if((self.currentTime-self.initialTime) > 500):
                 dataPacket = json.dumps(vars(self.currentState),sort_keys=True,indent=4)
                 self.request.send(dataPacket)
-                self.initialTime = int(time.time()*1000.0)
+                self.initialTime = self.currentTime
                 print dataPacket
-            '''
 
     def finish(self):
         self.request.close()
