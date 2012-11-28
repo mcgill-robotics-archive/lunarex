623f12cbdf7c498f13fbadeab172167f167dfb19
Hadi Eddy Sayar <hadi.sayar@mail.mcgill.ca>
2012-11-28 15:12:54 -0500

    Updated GUIMain and created Arena.java

--- Comms/LunarEx_Client/src/lunarex/gui/GUIMain.java
+++ Comms/LunarEx_Client/src/lunarex/gui/GUIMain.java
@@ -13,25 +13,21 @@ import lunarex.network.*;
 
 public class GUIMain extends JFrame {
 
-	
-
 	private static final long serialVersionUID = 1190876640530075607L;
 	static final int WIDTH = 1280;
 	static final int HEIGHT = (int) (9 / 16.0 * WIDTH);
 
-
 	String ipAdressString = "142.157.37.110";
-	
+
 	String portNumberString = "5902";
-	
+
 	Client client = null;
 	boolean connected = false;
-	
-	
 
+	// hard-coded some pieces of information about the environment, such as width of Mining Area and initial x value of Obstacle Area
 	class Bob {
-		
-		float x, y, w, h, velocity;
+
+		float x, y, w = 75, h = 100, velocity;
 		int angle, containerAngle;
 		float massInContainer, totalMass;
 		int batteryLevel = 50;
@@ -40,21 +36,25 @@ public class GUIMain extends JFrame {
 		int elevationWheel1, elevationWheel2, elevationWheel3, elevationWheel4;
 
 	}
-	class Crater{
-		double x,y,rA,rB;
+
+	class Crater {
+		double x, y, rA, rB;
 	}
-	class Boulder{
-		double x,y,rA,rB;
+
+	class Boulder {
+		double x, y, rA, rB;
 	}
-	
-	class MiningArea{
-		double w;
+
+	class MiningArea {
+		double w = 294;
 	}
-	class ObstacleArea{
-		Boulder rockA, rockB;
+
+	class ObstacleArea {
+		Boulder rockA, rockB, rockC;
 		Crater craterA, craterB;
-		double w;
+		double x = 150;
 	}
+
 	class Rect {
 		double x, y, w, h;
 	}
@@ -68,12 +68,11 @@ public class GUIMain extends JFrame {
 	byte[] outByte = new byte[1];
 	Boulder rockNo1 = new Boulder();
 	Boulder rockNo2 = new Boulder();
+	Boulder rockNo3 = new Boulder();
 	Crater craterOne = new Crater();
 	Crater craterTwo = new Crater();
 	ObstacleArea obstacleArea1 = new ObstacleArea();
 	MiningArea miningArea = new MiningArea();
-	
-	
 
 	public GUIMain() {
 
@@ -82,16 +81,16 @@ public class GUIMain extends JFrame {
 		canvas = new Canvas();
 		canvas.setIgnoreRepaint(true);
 		canvas.setSize(WIDTH, HEIGHT);
+		
 		add(canvas);
 		pack();
-		
 
 		// Hookup keyboard polling
 		addKeyListener(keyboard);
 		canvas.addKeyListener(keyboard);
 
-		bob.w = 75
-		bob.h = 100;
+		//bob.w = 75;
+		//bob.h = 100;
 		field.w = 738 * 1;
 		field.h = 388 * 1;
 		field.x = WIDTH - field.w - 20;
@@ -101,11 +100,12 @@ public class GUIMain extends JFrame {
 		manualOverride = true;
 
 	}
-	public void updateData(){
+
+	public void updateData() {
 		bob.x = (float) field.x + client.getX();
 		bob.y = (float) field.y + client.getY();
 		bob.angle = (int) client.getTheta();
-		
+
 		/*
 		 * bob.velocity = client.getVelocity();
 		
@@ -120,9 +120,9 @@ public class GUIMain extends JFrame {
 			bob.rpm4 = client.getRpm4();
 		 * 
 		 */
-		
+
 	}
-	
+
 	public void run() {
 
 		canvas.createBufferStrategy(2);
@@ -139,17 +139,13 @@ public class GUIMain extends JFrame {
 
 		while (true) {
 			try {
-				//System.out.println(outByte[0]);
+				// System.out.println(outByte[0]);
 				if (outByte[0] != 0) {
-<<<<<<< HEAD
-					//client.send(outByte);
-=======
-					if(client != null)
-					{
+					// client.send(outByte);
+					if (client != null) {
 						client.send(outByte);
-						//System.out.println(outByte[0]);
+						// System.out.println(outByte[0]);
 					}
->>>>>>> e2b5f3acf0971edbeae0cb17839f23f801c2e560
 					for (int i = 0; i < outByte.length; i++) {
 						outByte[i] = 0;
 					}
@@ -176,20 +172,31 @@ public class GUIMain extends JFrame {
 				g2d.drawString("Angle: "
 						+ (int) (360 * bob.angle / (2 * Math.PI)), 20, 64);
 				g2d.drawString("Manual Override: " + manualOverride, 20, 88);
-				g2d.drawString("Connected to server: "+connected, 20, 100);
+				g2d.drawString("Connected to server: " + connected, 20, 100);
 
 				g2d.drawString("(0,0)", (int) (field.x - 25),
 						(int) (field.y - 2));
 
 				// Move bob
 				processInput();
-				
+
 				// Draw Rectangular Field
 				g2d.setColor(Color.cyan);
 				g2d.drawRect((int) (field.x), (int) (field.y), (int) field.w,
 						(int) field.h);
 				
-				//Draw battery
+				// Draw MiningArea on Field
+				g2d.setColor(Color.white);
+				g2d.draw(new Line2D.Double(field.x + field.w - miningArea.w,
+						field.y, field.x + field.w - miningArea.w, field.y
+								+ field.h));
+
+				// Draw ObstacleArea on Field
+				g2d.setColor(Color.pink);
+				g2d.draw(new Line2D.Double(field.x + obstacleArea1.x, field.y,
+						field.x + obstacleArea1.x, field.y + field.h));
+	
+				// Draw battery
 				g2d.setColor(Color.WHITE);
 				g2d.drawRect(1000, 50, 100, 33);
 				g2d.drawRect(1100, 61, 5, 10);
@@ -233,7 +240,16 @@ public class GUIMain extends JFrame {
 			}
 		}
 	}
+	//Teleport bob to position x,y
+	public void teleport(Bob b, int x, int y, int theta){
+		b.x = (float) field.x + client.getX() + x;
+		b.y = (float) field.y + client.getY() + y;
+		bob.angle = theta;
+	}
 
+	public void moveForward(Bob b, int x){
+		b.x += x;
+	}
 	// keyboard input
 	protected void processInput() {
 		if (manualOverride) {
@@ -253,20 +269,20 @@ public class GUIMain extends JFrame {
 			if (keyboard.keyDown(KeyEvent.VK_RIGHT)) {
 				outByte[0] |= 1 << 3;
 			}
-			if(keyboard.keyDown(KeyEvent.VK_CONTROL))
-			{
+			if (keyboard.keyDown(KeyEvent.VK_CONTROL)) {
 				outByte[0] |= 1 << 4;
 			}
 		}
 		// IP ADDRESS BOX
-		if (keyboard.keyDownOnce(KeyEvent.VK_SPACE)&&!connected) {
-			client = new Client(ipAdressString,Integer.parseInt(portNumberString));
+		if (keyboard.keyDownOnce(KeyEvent.VK_SPACE) && !connected) {
+			client = new Client(ipAdressString,
+					Integer.parseInt(portNumberString));
 			client.start();
 			connected = true;
 		}
 		// Clear circles if they press C
 		if (keyboard.keyDownOnce(KeyEvent.VK_C)) {
-			
+
 		}
 		if (keyboard.keyDownOnce(KeyEvent.VK_O)) {
 			int reply = JOptionPane.showConfirmDialog(null,
