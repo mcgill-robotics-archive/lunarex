5b721c2588ec8fd40cfabbaf49c6758de8579c32
McGillLunarEx <nkhoi.tran@gmail.com>
2013-01-12 21:17:32 +0000

    ROS_server works

---
+++ ROS_workspace/lx_server/scripts/listener.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('lx_server')
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo(rospy.get_name() + ": I heard %s" % ord(data.data))
+
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("commands", String, callback)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        listener()
+    except KeyboardInterrupt:
+        sys.exit(0)
