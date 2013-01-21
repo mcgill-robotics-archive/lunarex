a264a1ff181ae16cd77c67feab9c33f5029b3fa1
everfor <alan.yang1994@gmail.com>
2013-01-21 16:33:52 -0500

    Test on python nodes

--- ROS_workspace/lx_server/scripts/server_listener.py
+++ ROS_workspace/lx_server/scripts/server_listener.py
@@ -6,15 +6,11 @@ from std_msgs.msg import Int8
 def callback(data):
     rospy.loginfo(rospy.get_name() + ": I heard %s" % data.data)
 
-
-def listener():
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("commands", Int8, callback)
-    rospy.spin()
-
-
 if __name__ == '__main__':
     try:
-        listener()
+        rospy.init_node('listener', anonymous=True)
+        rospy.Subscriber("commands", Int8, callback)
+        rospy.spin()
+        print "Can you see this line?\n"
     except KeyboardInterrupt:
         sys.exit(0)
