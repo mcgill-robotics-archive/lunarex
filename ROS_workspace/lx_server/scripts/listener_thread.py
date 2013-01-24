fa3c3c4102288e04064413ec961017476341c652
McGillLunarEx <nkhoi.tran@gmail.com>
2013-01-24 20:31:28 +0000

    Multi node

--- ROS_workspace/lx_server/scripts/listener_thread.py
+++ ROS_workspace/lx_server/scripts/listener_thread.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('lx_server')
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import *
 import threading
 
 thread_data = None
 
 def callback(data):
     thread_data = data.data
+    print "I heard %s" % data.data
 
 def getData():
     return thread_data
-    
-class listerThread(threading.Thread):
+
+class listenerThread(threading.Thread):
     def __init__(self, NODE_NAME, topic):
-        rospy.init_node(NODE_NAME, anonymous = True)
-        rospy.Subscriber(topic, Int8, callback)
-    
+        print "Initiating thread...\n"
+        self.NODE_NAME = NODE_NAME
+        self.topic = topic
+        rospy.init_node(self.NODE_NAME, anonymous = True)
+        rospy.Subscriber(self.topic, Int8, callback)
+        print "ROS subscriber initiated...\n"
+        threading.Thread.__init__(self)
+
     def run(self):
-        rospy.spin()
+        try:
+            print "Currently running ROS subscriber thread...\n"
+            rospy.spin()
+        except KeyboardInterrupt:
+            sys.exit(0)
--- ROS_workspace/lx_server/scripts/server_listener_test.py
+++ ROS_workspace/lx_server/scripts/server_listener_test.py
@@ -3,25 +3,37 @@ import roslib; roslib.load_manifest('lx_server')
 import rospy
 from std_msgs.msg import Int8
 import threading
-import lister_thread as lthread
+import listener_thread as lthread
+import time
 
 data = None
+initialTime = time.time()
 
 def printMessage():
-    data = lthread.getData()
-    if(data <> None):
-        print "Data is %s" % data
+    while True:
+        currentTime = time.time()
+        if(currentTime - initialTime == 500):
+            print "Hello I'm running...\n"
+            data = lthread.getData()
+            if(data <> None):
+                print "Data is %s" % data
 
 if __name__ == '__main__':
     try:
+        print "Running the main program...\n"
+        #initialTime = time.time()
         thread1 = lthread.listenerThread("listenerThread", "commands")
         thread2 = threading.Thread(target = printMessage)
+        print "2 threads created...\n"
+        print "Thread 1 started...\n"
         thread1.start()
+        print "Thread 2 started...\n"
         thread2.start()
         threadList = []
         threadList.append(thread1)
         threadList.append(thread2)
         for t in threadList:
             t.join()
+            print "1 thread joined"
     except KeyboardInterrupt:
         sys.exit(0)
