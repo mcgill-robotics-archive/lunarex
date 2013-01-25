f4ae527b52b1b0e2c0d6a5fa30821c2d96cc6e3e
everfor <alan.yang1994@gmail.com>
2013-01-24 19:45:06 -0500

    More...

--- ROS_workspace/lx_server/scripts/server_listener_test.py
+++ ROS_workspace/lx_server/scripts/server_listener_test.py
@@ -14,11 +14,9 @@ def callback(data):
     print "I heard %s" % thread_data
 
 class listenerThread(threading.Thread):
-    def __init__(self, NODE_NAME, topic):
+    def __init__(self, topic):
         print "Initiating thread...\n"
-        self.NODE_NAME = NODE_NAME
-        self.topic = topic
-        rospy.Subscriber(self.topic, Int8, callback)
+        rospy.Subscriber(topic, Int8, callback)
         print "ROS subscriber initiated...\n"
         threading.Thread.__init__(self)
 
@@ -63,7 +61,7 @@ if __name__ == '__main__':
         print "Running the main program...\n"
         rospy.init_node(NODE_NAME, anonymous = True)
         #initialTime = time.time()
-        thread1 = listenerThread("listenerThread", "commands")
+        thread1 = listenerThread("commands")
         #thread2 = threading.Thread(target = printMessage)
         thread2 = publisherThread()
         print "2 threads created...\n"
