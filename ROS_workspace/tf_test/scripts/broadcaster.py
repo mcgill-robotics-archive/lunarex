47392fe77b26975a7186d4486fc304e349d744c7
McGillLunarEx <nkhoi.tran@gmail.com>
2013-02-10 19:33:59 +0000

    further test on tf

--- ROS_workspace/tf_test/scripts/broadcaster.py
+++ ROS_workspace/tf_test/scripts/broadcaster.py
@@ -1,17 +1,14 @@
+#!/usr/bin/env python
 import roslib; roslib.load_manifest('tf_test')
 import rospy
 import tf
+import sys
 
-BR = None
-RATE = None
-
-def init_broadcaster():
-    rospy.init_node('test_broadcaster')
-    BR = tf.TransformBroadcaster()
-    RATE = rospy.rate(10)
 
 if __name__ == '__main__':
-    init_broadcaster()
+    rospy.init_node('test_broadcaster')
+    BR = tf.TransformBroadcaster()
+    RATE = rospy.Rate(10)
     try:
         while not rospy.is_shutdown():
             BR.sendTransform((1.0, 1.0, 1.0),
@@ -19,6 +16,8 @@ if __name__ == '__main__':
                             rospy.Time.now(),
                             "child",
                             "parent")
+            print 'Sended 1'
             RATE.sleep()
+        print 'Out of the loop'
     except KeyboardInterrupt:
         sys.exit(0)
--- ROS_workspace/tf_test/scripts/listener.py
+++ ROS_workspace/tf_test/scripts/listener.py
@@ -1,19 +1,17 @@
-import roslib; roslib.manifest('tf_test')
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('tf_test')
 import rospy
 import tf
+import sys
 
-LS = None
 
-def init_listener():
-    rospy.init_node('test_listener')
-    LS = tf.tf.TransformListener()
-    
 if __name__ == '__main__':
-    init_listener()
+    rospy.init_node('test_listener')
+    LS = tf.TransformListener()
     try:
         while not rospy.is_shutdown():
-            now = rospy.Time.now() - rospy.Duration(5.0)
-            LS.waitForTransformation('child', 'parent', now, rospy.Duration(10.0))
+            now = rospy.Time.now()
+            #LS.waitForTransform('child', 'parent', now, rospy.Duration(10.0))
             (trans, rot) = LS.lookupTransform('child', 'parent', now)
             print 'find something' + trans + rot
     except KeyboardInterrupt:
