4dcbebe8f3bb8a564a034e1db739750d2a272162
everfor <alan.yang1994@gmail.com>
2013-02-10 14:36:22 -0500

    further

--- ROS_workspace/tf_test/scripts/broadcaster.py
+++ ROS_workspace/tf_test/scripts/broadcaster.py
@@ -8,11 +8,11 @@ import sys
 if __name__ == '__main__':
     rospy.init_node('test_broadcaster')
     BR = tf.TransformBroadcaster()
-    RATE = rospy.Rate(10)
+    RATE = rospy.Rate(10.0)
     try:
         while not rospy.is_shutdown():
             BR.sendTransform((1.0, 1.0, 1.0),
-                            tf.transformations.quaternion_from_euler(0, 0, 1),
+                            (0.0, 0.0, 0.0, 1.0),
                             rospy.Time.now(),
                             "child",
                             "parent")
