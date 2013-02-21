7fcc93671f10dfce0cb2330db74665b394e7d6d2
McGillLunarEx <nkhoi.tran@gmail.com>
2013-02-21 05:33:09 +0000

    further map_builder
    Former-commit-id: a1b115798186a05ce14e67bd09128bf8525495a0

--- ROS_workspace/map_builder/src/map_builder.py
+++ ROS_workspace/map_builder/src/map_builder.py
@@ -23,15 +23,17 @@ class mapBuilder:
         #self.new_map = self.map
         self.getMapParameters()
         self.pub.publish(self.map)
-        
+
     #Retrieve position data
     def poseCallback(self, pose):
         self.position = pose
         self.x_position = self.position.pose.position.x
         self.y_position = self.position.pose.position.y
-        print "Current position:" 
+        print "Current position:"
         print self.x_position
         print self.y_position
+        print self.position.pose.orientation
+        #print self.position.pose.orientation.w
 
     def addCoordinates(x, y):
         self.obstacle_list.append([x,y])
@@ -42,18 +44,19 @@ class mapBuilder:
         rotated_vect.append( vector[0]*math.cos(angle) - math.sin(angle) )
         rotated_vect.append( vector[1]*math.sin(angle)  + vector[1]*math.cos(angle) )
         return rotated_vect
-        
+
     def getMapParameters(self):
         self.map_height = self.map.info.height  #the height of occupancy gird
         self.map_width = self.map.info.width    #the width of occupancy grid
         self.map_resolution = self.map.info.resolution
-        
+
     def processMap(self):
         pass    #implent later
-    
+
     def insertValueInOccupancyGrid(self, x_coord, y_coord, val):
-        self.map.data[]
-    
+        #self.map.data[]
+        pass
+
 if __name__ == "__main__":
     try:
         builder = mapBuilder()
