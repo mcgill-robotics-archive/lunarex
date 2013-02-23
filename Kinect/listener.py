b57218f6fdf338feaf89160de9a784514cc04c96
nickspeal <nickspeal@gmail.com>
2013-02-22 20:21:11 -0500

    Added some comments
    Former-commit-id: 966e52998771c0f726351409e284131e2742fe29

--- Kinect/listener.py
+++ Kinect/listener.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
+'''
+The convention in Python is to describe what this module (program) does here.
+'''
+
+
 import roslib; roslib.load_manifest('') #add directory
 import rospy
 for sensor_msgs import image		#What does this do? - Nick
 import numpy as np
 
 def depthMatrixReshape(depthValues):
+    '''
+    The convention in Python is to describe how a method works here.
+    '''
+    
     x = depthValues.step
     y = depthValues.row
     depthArray = [[for i in range(x)] for j in range(y)]
@@ -19,5 +28,5 @@ rospy.spin()
 
 # A Couple Notes by Nick
 #Can you please rename this module to something more specific?
-
 #make sure you use consistent indentation in python
+#please add comments to clarify how this module works.
