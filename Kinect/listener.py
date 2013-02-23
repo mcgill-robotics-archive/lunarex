b18125566cc5fe10149660ab14d722a054ab2a25
nickspeal <nickspeal@gmail.com>
2013-02-22 20:17:55 -0500

    Added some comments looking for clarification, and cleaned up some indentation
    Former-commit-id: f618f057e642e71098d5fc4e86e812b5ae317071

--- Kinect/listener.py
+++ Kinect/listener.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('') #add directory
 import rospy
-for sensor_msgs import image
+for sensor_msgs import image		#What does this do? - Nick
 import numpy as np
 
 def depthMatrixReshape(depthValues):
-   x = depthValues.step
-	 y = depthValues.row
-	 depthArray = [[for i in range(x)] for j in range(y)]
-	 depthArray = np.reshape(depthValues.data, (x, y))
+    x = depthValues.step
+    y = depthValues.row
+    depthArray = [[for i in range(x)] for j in range(y)]
+	depthArray = np.reshape(depthValues.data, (x, y))
 	 
 
 
 rospy.init_node('depthListener', anonymous=True)
 rospy.Subscriber('depth/image_raw', image, depthMatrixReshape)
 rospy.spin()
+
+
+# A Couple Notes by Nick
+#Can you please rename this module to something more specific?
+
+#make sure you use consistent indentation in python
