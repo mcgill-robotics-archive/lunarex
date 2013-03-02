b231d0b56131cb1cb94a1dab7e758fe7ef44966c
SebastienLC <seb.lemieux.codere@gmail.com>
2013-03-02 14:52:24 -0500

    service test
    Former-commit-id: f0dd93d1d1c9d92af3b11f4e288140bf38280feb

---
+++ ROS_workspace/tf_service_test/scripts/srv_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('tf_service_test')
+
+import sys
+
+import rospy
+from tf_service_test.srv import *
+
+import numpy as np
+
+
+if __name__ == "__main__":
+    try:
+	rospy.init_node('test_service_client')
+	rate = rospy.Rate(0.5)
+	while not rospy.is_shutdown:
+	    rospy.wait_for_service('test_service')
+	    try:
+		service_resp = rospy.ServiceProxy('test_service', TestSrv)
+		response = service_resp("request")
+		print "height: %d" % response.height
+		print "width: %d" % response.width
+
+		print response.kinect_map
+	    except rospy.ServiceException, e:
+		print "Service call failed: %s" % e
---
+++ ROS_workspace/tf_service_test/scripts/srv_server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('tf_service_test')
+
+from tf_service_test.srv import *
+import rospy
+import sys
+import numpy as np
+
+test_matrix = np.array([[1,2],[3,4],[5,6]], np.int32)
+
+def handle_add_two_ints(req):
+    print "Returning kinect data matrix"
+    return TestSrvResponse(a.shape[0], a.shape[1], np.reshape[a, -1])
+
+def service_server():
+    s = rospy.Service('test_service', TestSrv, handle_service)
+    print "Test Server Ready."
+    rospy.spin()
+
+if __name__ == "__main__":
+    try:
+	rospy.init_node("test_srv_server")
+	service_server()
+    except KeyboardInterrupt:
+	sys.exit(0)
