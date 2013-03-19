178e34ee7a3330e5433467abaef1c1870831cc49
ernopp <eoppetit@gmail.com>
2013-03-19 17:07:37 -0400

    changes to map_builder_live launchers
    Former-commit-id: 128cc7fd8f11382b566c95c72a10c7e0773a75d3

--- ROS_workspace/map_builder/map_builder_live.sh
+++ ROS_workspace/map_builder/map_builder_live.sh
@@ -11,5 +11,9 @@ xterm -e roscore
 echo "Setting hokuyo_node port"
 rosparam set hokuyo_node/port /dev/ttyACM0 
 
+echo "Launching all the ROS stuff.. hokuyo_node, hector_mapping, kinect_srv & map_builder"
+roslaunch map_builder_live.launch
 
+echo "Starting rviz"
+xterm -e rosrun rviz rviz -d $(find hector_slam_launch)/rviz_cfg/mapping_demo.vcg"
 
