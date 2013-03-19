28d9802d3d9d912e5df23758c99b1646df2a8f20
McGillLunarEx <nkhoi.tran@gmail.com>
2013-03-19 23:36:14 +0000

    finished hector_mapping launch file
    Former-commit-id: 3c7a24cb457b474b631712ad01bef02d4864def0

--- ROS_workspace/map_builder/map_builder_live.sh
+++ ROS_workspace/map_builder/map_builder_live.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
-#RUN AS SUDO!
+#NOTES
+#Will ask for password for sudo
+#Do not forget the & after an xterm command
+#Relies on the followign files:
+#   - ../hector_launchers/l2.launch
+#   - ../rviz_configs/hector_mapping_demo.vcg
+#   kinect_srv_server.py in kinect_node package
+#   map_builder.py in map_builder package
 
-echo "setting Lidar port permissions.."
-chmod a+rw /dev/ttyACM0
+echo "***Kill anything that's already running"
+killall roscore
+killall hokuyo_node
 
-echo "Starting roscore"
-xterm -e roscore
+echo "***setting Lidar port permissions.."
+sudo chmod a+rw /dev/ttyACM0
 
-echo "Setting hokuyo_node port"
+echo "***Starting roscore"
+xterm -e roscore &
+
+echo "***sleep a bit to wait for roscore"
+sleep 5
+
+echo "***Setting hokuyo_node port"
 rosparam set hokuyo_node/port /dev/ttyACM0 
 
-echo "Launching all the ROS stuff.. hokuyo_node, hector_mapping, kinect_srv & map_builder"
+echo "***set rosparam to get rid of annoying warning"
+rosparam set use_rep_117 true 
+
+echo "***Running hokuyo_node"
+xterm -e rosrun hokuyo_node hokuyo_node & 
+
+sleep 3
+
+echo "***Running hector_mapping"
+xterm -e roslaunch ../hector_launchers/l2.launch &
+
+echo "***Starting rviz"
+xterm -e rosrun rviz rviz -d ../rviz_configs/hector_mapping_demo.vcg
+
+echo "***Launching kinect and map builder"
 roslaunch map_builder_live.launch
-
-echo "Starting rviz"
-xterm -e rosrun rviz rviz -d $(find hector_slam_launch)/rviz_cfg/mapping_demo.vcg"
-
