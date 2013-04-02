a4075976d66f33469fdbaeda491060086dd31c02
McGillLunarEx <nkhoi.tran@gmail.com>
2013-04-02 20:46:16 +0000

    adding arduino sketchbook
    Former-commit-id: 81aa84a606f04cd46b9c497331a3f1162e779470

--- LidarEx/occupancyGrid_parserv3.4.py
+++ LidarEx/occupancyGrid_parserv3.4.py
@@ -57,9 +57,11 @@ def sameBuckets(l1, l2):
 	return (sameRBucket(l1.r, l2.r) and sameTBucket(l1.theta, l2.theta))
 
 #BAG SETUP
-bag = rosbag.Bag('/home/ernie/Dropbox/LunarEx2012-2013/Software Team/ROS/Bags/localization_feb10.bag')
+#bag = rosbag.Bag('/home/ernie/Dropbox/LunarEx2012-2013/Software Team/ROS/Bags/localization_feb10.bag')
 ##DO NOT USE THIS BAG. IT SUCKS. bag = rosbag.Bag('/home/ernie/Dropbox/LunarEx2012-2013/Software Team/ROS/Bags/2013-03-19-14-26-50.bag') #shitty
 
+#netbook bag
+bag = rosbag.Bag('basic_localization_stage.bag')
 #DO NOT USE THIS BAG. IT SUCKS. bag = rosbag.Bag('/home/ernie/Dropbox/LunarEx2012-2013/Software Team/ROS/Bags/2013-03-19-14-21-47.bag') #shitty
 
 #bag = rosbag.Bag('/home/ernie/Dropbox/LunarEx2012-2013/Software Team/ROS/Bags/2013-02-20-21-37-14.bag') # awesome stage bag
