fixed coordinate detector vector rotation. THe goals are now good with all starting orientations; navigation doesn't get to some of them though.
Former-commit-id: 6b4693f183b0c0194fba1787d9267e0e8acb4ae8

ROS_workspace/command/src/command_node.py
@@ -37,8 +37,8 @@ ARENA_LENGTH = 7.38
 MINING_BOUNDARY_LOCATION = 4.44  # distance of boundary to lunabin
 
 STARTING_POS_ARENA_COORDS = [(0.97, 0.75), (2.91, 0.75)]
-ROTATION_TIME_SECS = 10
-LOCALIZATION_ANG_SPEED = 0.2
+ROTATION_TIME_SECS = 35
+LOCALIZATION_ANG_SPEED = 0.1
 
 #--state vars
 #----corners
@@ -225,7 +225,7 @@ def setAugerSpeed(desiredSpeed):
 def goTo(x,y,theta):
 	# send a specified goal in a compact form
 	# All three parameters in Arena coordinates
-	rospy.loginfo("In goTO with heading: " +str(quatToDegrees(slam_out_pose)))
+	rospy.loginfo("In goTO with heading: " +str(coord.quatToDegrees(slam_out_pose)))
 
 	nextGoal = coord.arena2mobile((x,y), slam_out_pose, LR_corner, RR_corner, RF_corner, LF_corner, mapRes, mapWidth)
 	#(arenaCoords, slam_out_pose, corner1, corner2, corner3, corner4, resolution):
@@ -237,21 +237,15 @@ def goTo(x,y,theta):
 	#must convert to quaternion from a rad
 	quat = tf.transformations.quaternion_from_euler(0, 0, mobileAngle*math.pi/180.0) #was 0, 0, math.pi
 	#quat = tf.transformations.quaternion_from_euler(0, 0, 0) #was 0, 0, math.pi
+
 	goal.target_pose.pose.orientation = Quaternion(*quat)
 	
 	rospy.loginfo("***Requested motion***")
 	rospy.loginfo("x = " +str(goal.target_pose.pose.position.x))
 	rospy.loginfo("y = " +str(goal.target_pose.pose.position.y))
+	rospy.loginfo("theta = " +str(mobileAngle))
 	client.send_goal(goal)  # Sends the goal to the action server.
 	client.wait_for_result() # Waits for the server to finish performing the action.
-
-	# goal.target_pose.pose.position.x = 0
-	# goal.target_pose.pose.position.y = 0
-	# quat = tf.transformations.quaternion_from_euler(0, 0, mobileAngle*math.pi/180.0)
-	# goal.target_pose.pose.orientation = Quaternion(*quat)
-	# client.send_goal(goal)
-	# rospy.loginfo("got to destination. Now requesting angle:")
-	# rospy.loginfo("theta = " +str(mobileAngle))
 
 class Velocity:
     def __init__(self, x, y, z):
@@ -308,10 +302,13 @@ rospy.loginfo("Done waiting for move_base action server")
 goal.target_pose.header.frame_id = "base_link"
 goal.target_pose.header.stamp = rospy.get_rostime()
 
-# goal.target_pose.pose.position.x = 0.0
-# goal.target_pose.pose.position.y = 0.0 
-# quat = tf.transformations.quaternion_from_euler(0, 0, math.pi) #was 0, 0, math.pi
-# goal.target_pose.pose.orientation = Quaternion(*quat)
+#goal.target_pose.pose.position.x = 2.0
+#goal.target_pose.pose.position.y = 1.0 
+#quat = tf.transformations.quaternion_from_euler(0, 0, 45*math.pi/180.0 ) #was 0, 0, math.pi
+#goal.target_pose.pose.orientation = Quaternion(*quat)
+
+#client.send_goal(goal)  # Sends the goal to the action server.
+#client.wait_for_result() # 
 
 #Go to start of mining area
 goTo(ARENA_WIDTH/2.0, MINING_BOUNDARY_LOCATION, 0)
@@ -320,7 +317,7 @@ goTo(ARENA_WIDTH/2.0, MINING_BOUNDARY_LOCATION, 0)
 #excavate()
 
 #Return home and dump
-#goTo(ARENA_WIDTH/2.0, 0.9, math.pi)	#need to callibrate y position so as not to bump into wall or obstacle
-#goTo(ARENA_WIDTH/2.0, 0.9, math.pi)	#spin around
+goTo(ARENA_WIDTH/2.0, 0.9, math.pi)	#need to callibrate y position so as not to bump into wall or obstacle
+goTo(ARENA_WIDTH/2.0, 0.9, math.pi)	#spin around
 
 

ROS_workspace/coord.py
@@ -35,6 +35,11 @@ def quatToDegrees(slam_out_pose):
   angles = tf.transformations.euler_from_quaternion([slam_out_pose.pose.orientation.x,slam_out_pose.pose.orientation.y, slam_out_pose.pose.orientation.z, slam_out_pose.pose.orientation.w])
   return angles[2]*(180.0/math.pi)
 
+def quatToRadians(slam_out_pose):
+  angles = tf.transformations.euler_from_quaternion([slam_out_pose.pose.orientation.x,slam_out_pose.pose.orientation.y, slam_out_pose.pose.orientation.z, slam_out_pose.pose.orientation.w])
+  return angles[2]
+
+
 #POSITION TRANSFORMS
 def arena2mobile(arenaCoords, slam_out_pose, LR_corner, RR_corner, LF_corner, RF_corner, resolution, global_map_size):
   # step1: transform arenaX and arenaY to global frame  - these are goals
@@ -44,16 +49,14 @@ def arena2mobile(arenaCoords, slam_out_pose, LR_corner, RR_corner, LF_corner, RF
   (goal_x_global, goal_y_global) = arena2global(arenaCoords, LR_corner, RR_corner, LF_corner, RF_corner, resolution)
   (current_x_global, current_y_global) = hector2global(slam_out_pose, resolution, global_map_size)
 
-  theta = math.atan2(abs(LR_corner[1]-RR_corner[1]), abs(LR_corner[0]-RR_corner[0]))  #angle of bottom wall of arena w.r.t. the positive global x axis
-
-  goal_x_mobile = (goal_y_global - current_y_global)*resolution
-  goal_y_mobile = (current_x_global - goal_x_global)*resolution
+  goal_x_mobile = (goal_x_global - current_x_global)*resolution
+  goal_y_mobile = (goal_y_global - current_y_global)*resolution
 
   #this is a vector in global
   mobile_coords = (goal_x_mobile, goal_y_mobile)
 
   #rotate the vector from global to hector
-  mobile_coords = rotateVector2D(mobile_coords, 90-quatToDegrees(slam_out_pose))
+  mobile_coords = rotateVector2D(mobile_coords, -quatToRadians(slam_out_pose))
 
   rospy.loginfo("coord.arena2mobile received: "+str(arenaCoords)+" and is returning: "+str(mobile_coords))
   return mobile_coords
@@ -64,12 +67,10 @@ def hector2global(slam_out_pose, resolution, global_map_size):
   x_hector = slam_out_pose.pose.position.x
   y_hector = slam_out_pose.pose.position.y
 
-  #translate global frame origin to middle of map
-  x_temp = x_hector + global_map_size*resolution/2
-  y_temp = y_hector - global_map_size*resolution/2
+  #translate global frame origin to middle of mapt
 
-  x_global = -1*y_temp / resolution #resolution in m/cell, see documentation on google site for logic here
-  y_global = x_temp / resolution
+  x_global = global_map_size / 2 + x_hector / resolution #resolution in m/cell, see documentation on google site for logic here
+  y_global = global_map_size / 2 + y_hector / resolution
 
   global_coords = (x_global, y_global)
   rospy.loginfo("coord.hector2global received: x_hector="+str(x_hector)+" & y_hector="+str(y_hector)+" and is returning: "+str(global_coords))
@@ -148,7 +149,12 @@ def isInObstacleArea(globalCoords, LR_corner, RR_corner, LF_corner, RF_corner, r
 
  #Rotate a coordinate ector
 def rotateVector2D(vector, angle):
+  print "--"
+  print "rotating vector"
+  print "input vector: " + str(vector)
+  print "rotation angle: " + str(angle)
   rotated_vect = []
   rotated_vect.append(  vector[0]*math.cos(angle)  - vector[1]*math.sin(angle)             )
   rotated_vect.append(  vector[0]*math.sin(angle)  + vector[1]*math.cos(angle)   )
+  print "output vector: " + str(rotated_vect)
   return rotated_vect