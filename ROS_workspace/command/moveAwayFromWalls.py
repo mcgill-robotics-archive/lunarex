0258fa971b85cb6b5dc827da6953888448681a2c
nickspeal <nickspeal@gmail.com>
2013-05-16 00:51:02 -0400

    added fine tuning to calibrate ino file. Made some progress skeletoning out the moveAwayFromWalls
    Former-commit-id: b0ab820219ae3b89df6247cab9f0bfa64bfd8343

--- ROS_workspace/command/moveAwayFromWalls.py
+++ ROS_workspace/command/moveAwayFromWalls.py
@@ -1,2 +1,52 @@
-def moveAwayFromWalls()
+def getDistances():
+
+
+	return leftDistance, frontDistance, rightDistance
+
+
+def moveAwayFromWalls():
+	'''
+		Assume we start in the bottom left corner for this description. The code is general and would work in either corner
+		Our orientation could be in one of 4 quadrants
+		Describe these quadrants with regular math conventions, ie 0 deg. is positive arena x, quandrant number increases CCW
+
+		if we start in the bottom right, then the quadrants are mirrord about the y-axis ie quadrant 3 is in the corner, increasing CW
+
+
+
+	'''
+
+	leftDistance, frontDistance, rightDistance = getDistances()
+
+
+	# http://www.ros.org/doc/api/sensor_msgs/html/msg/LaserScan.html
+	# get distance values for 0, 90, 180 deg.
+
+
+
+	THRESHOLD = 3.0 # meter
+
+	if (frontDistance > THRESHOLD):
+		#facing away from walls, Quadtant 1
+		#drive forwards
+		#publish positive linear command velocity
+		quadrant = 1
+		pass
+	elif (rightDistance > THRESHOLD):
+		# quadrant 2
+		quadrant = 2
+		pass
+	elif (leftDistance > THRESHOLD):
+		# quadrant 4
+		# turn 90 degrees and do it again
+		quadrant = 4
+		pass
+	else:
+		# Facing the corner, Quadrant 3
+		# back up
+		quadrant = 3
+		pass
+
+	return quadrant
+
 
