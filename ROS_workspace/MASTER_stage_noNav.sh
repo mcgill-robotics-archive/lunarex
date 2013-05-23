35f35be85cc00f23c47b7b42a27e55880db378cc
SebastienLC <seb.lemieux.codere@gmail.com>
2013-05-22 22:50:58 -0400

    git push
    Former-commit-id: c846d64c266d5a733cf3782247b61b34cd227e51

--- ROS_workspace/MASTER_stage_noNav.sh
+++ ROS_workspace/MASTER_stage_noNav.sh
@@ -1,4 +1,9 @@
 killall xterm
+killall roscore
+killall hector_mapping
+killall python
+
+
 xterm -e  roslaunch lunarex_stage/stageLaunchV2.launch &
 xterm -e  roslaunch lunarex_2dnav/hector_launchers/simple_hector.launch &
 xterm -e  rosrun rviz rviz -d rviz_configs/hector_mapping_demo.vcg &
